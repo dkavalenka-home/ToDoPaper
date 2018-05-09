0b4e09969a9b1952d0ee779e6fadeab9f13ef61a
Dzmitry Kavalenka <zmiter.kavalenka@gmail.com>
2018-05-09 18:30:08 +0300

    Functions for ToDoPaper project

--- functions.js
+++ functions.js
@@ -1,31 +1,86 @@
-var todoItem = {
-	text: '',
-	completed: false,
-	id: 0,
+Array.prototype.addTodoItem = function (todoItem) {
+    if (typeof (todoItem.text) !== 'string' ||
+        typeof (todoItem.completed) !== 'boolean' ||
+        typeof (todoItem.id) !== 'number') {
+        console.error('This item is not todo item');
+        return;
+    };
+    if (!todoItem.text) {
+        console.error('Text field of todo item must be filled');
+        return;
+    }
+    if (this.some(item => item.id === todoItem.id)) {
+        console.error('Item wtih id: ' + todoItem.id + ' already exists');
+        return
+    }
+    this.push(todoItem);
 };
-function addTodoItem(text, completed, id) {
-	boxvalue = document.getElementById('box').value;
-		todoItems.push(boxvalue);
-			return true;
-				console.log(todoItems);
+
+Array.prototype.viewTodoList = function (state) {
+    switch (state) {
+        case 'complited': {
+            return this.filter(item => item.completed);
+        }
+        case 'not_complited': {
+            return this.filter(item => !item.completed);
+        }
+        case 'all': {
+            return this;
+        }
+        default: {
+            console.error('Incorrect parameter. You can use only : complited, not_complited, all');
+        }
+    }
+};
+
+Array.prototype.editTodoItem = function (todoItemId, newText) {
+    if (typeof (newText) !== 'string') {
+        console.error('Text field must be a string');
+        return false;
+    }
+    if (!newText) {
+        console.error('Text field of todo item must be filled');
+        return false;
+    }
+    if (typeof (todoItemId) !== 'number') {
+        console.error('Id field must be a number');
+        return false;
+    }
+    if (!this.some(item => item.id === todoItemId)) {
+        console.error('There is no item with id = ' + todoItemId + ' in collection');
+        return false;
+    }
+    this.find(item => item.id === todoItemId).text = newText;
+    return true;
 }
 
-var itemsType = '';
-function viewTodoList(itemsType) {
-	
+Array.prototype.deleteTodoItem = function (todoitemId) {
+    if (typeof (todoItemId) !== 'number') {
+        console.error('Id field must be a number');
+        return false;
+    }
+    if (!this.some(item => item.id === todoItemId)) {
+        console.error('There is no item with id = ' + todoItemId + ' in collection');
+        return false;
+    }
+    var index = this.findIndex(item => item.id === todoitemId);
+    this.splice(index, 1);
 }
 
-var todoItemId = 0;
-var newText = '';
-function editTodoItem(todoItemId, newText) {
-
-}
-
-function deleteTodoItem(todoItemId) {
-	delete todoItemId;
-	return flag = 1;
-}
-
-function completeTodoItem(todoItemId) {
-
+Array.prototype.completeTodoItem = function (todoItemId) {
+    if (typeof (todoItemId) !== 'number') {
+        console.error('Id field must be a number');
+        return false;
+    }
+    if (!this.some(item => item.id === todoItemId)) {
+        console.error('There is no item with id = ' + todoItemId + ' in collection');
+        return false;
+    }
+    var item = this.find(item => item.id === todoItemId);
+    if (item.completed) {
+        console.error('This item is already complited');
+        return false;
+    }
+    item.completed = true;
+    return true;
 }
--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 var todoItems = [];
+
+console.log('task 1');
+var todo1 = { text: 'kate is awesome', id: 1 };
+console.log(todo1);
+todoItems.addTodoItem(todo1);
+
+var todo2 = { text: '', completed: true, id: 1 };
+console.log(todo2);
+todoItems.addTodoItem(todo2);
+
+var todo3 = { text: 'pipi', completed: false, id: 1 };
+console.log(todo3);
+todoItems.addTodoItem(todo3);
+
+var todo4 = { text: 'miu', completed: false, id: 1 };
+console.log(todo4);
+todoItems.addTodoItem(todo4);
+
+console.log('task 2');
+todoItems.addTodoItem({ text: 'dimka idet gulat', completed: false, id: 2 });
+todoItems.addTodoItem({ text: 'dimka ne delaet raboty', completed: true, id: 3 });
+todoItems.addTodoItem({ text: 'dimka kushaet obed', completed: true, id: 4 });
+
+console.log('all items:');
+console.log(todoItems.viewTodoList('all'));
+
+console.log('complited items:');
+console.log(todoItems.viewTodoList('complited'));
+
+console.log('incomplited items:');
+console.log(todoItems.viewTodoList('not_complited'));
+
+console.log('wrong parameter:');
+console.log(todoItems.viewTodoList('dimka'));
+
+console.log('task 3');
+console.log(todoItems.editTodoItem(1, 'new text'));
+console.log(todoItems);
+
+console.log(todoItems.editTodoItem(1, 321));
+console.log(todoItems);
+
+console.log(todoItems.editTodoItem(45, 'good text'));
+console.log(todoItems);
+
+console.log('task 4');
+console.log(todoItems);
+
+console.log(todoItems.deleteTodoItem('123'));
+console.log(todoItems);
+
+console.log(todoItems.deleteTodoItem(77));
+console.log(todoItems);
+
+console.log(todoItems.deleteTodoItem(2));
+console.log(todoItems);
+
+console.log('task 5');
+console.log(todoItems);
+console.log(todoItems.completeTodoItem(2));
+console.log(todoItems);
+console.log(todoItems.completeTodoItem('1d2'));
+console.log(todoItems);
+console.log(todoItems.completeTodoItem(3));
+console.log(todoItems);
+console.log(todoItems.completeTodoItem(1));
